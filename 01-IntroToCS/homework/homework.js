7c259a52b7c4b9fe6c91e0a93be1796547a5cfaf
adripv7 <adrianapv9807@gmail.com>
2023-01-04 03:10:36 -0600

    tarea1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,29 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   console.log(num)
+   let sum = 0;
+   for (let i = 0; i < num.length; i++) {
+      sum += +num[i] * 2 ** (num.length - 1 - i)
+   }
 
-function DecimalABinario(num) {}
+   return sum;
+
+}
+
+function DecimalABinario(num) { 
+console.log (num)
+
+var resultado = "";
+while (num !==0){
+   resultado = (num%2) + resultado;
+   num = Math.floor (num/2)
+
+}
+
+return resultado;
+
+}
 
 module.exports = {
    BinarioADecimal,
